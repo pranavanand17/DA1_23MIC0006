3a886d00fb9093138923d530138ff98c356efebd
pranavanand17 <pranavisappu@proton.me>
2026-01-11 08:26:54 +0000

    23MIC0006
    Signed-off-by: pranavanand17 <pranavisappu@proton.me>

---
+++ JenkinsGitHubIntegration.java
@@ -0,0 +1,8 @@
+public class JenkinsGitHubIntegration {
+    public static void main(String[] args) {
+        System.out.println("Pranav Anand");
+        System.out.println("23MIC0006");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
+
